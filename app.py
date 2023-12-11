f9b85fd2cd4bf2de1ed5bb9b25690bc98d225006
Masha <test@test.com>
2023-12-11 21:24:47 +0300

    Подключение административной панели. Часть 1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from db.models import getAllProducts, getRangeProducts
 
 app = Flask(__name__)
@@ -15,4 +15,20 @@ def productsPage():
         minPrice = request.form['min']
         maxPrice = request.form['max']
         return render_template('products.html', products=getRangeProducts(minPrice, maxPrice))
-app.run(debug=True) 
+app.run(debug=True)
+
+@app.route("/admin")
+def adminPage():
+    # Перенаправление пользователя, еси он не зарегестрирован
+    if "login" not in session:
+        return redirect("/admin/login")
+
+@app.route("/admin/login", methods=["GET","POST"])
+def adminLoginPage():
+    if request.method == "GET":
+        return render_template('adminLogin.html')
+    elif request.method == "POST":
+        login = request.form["login"]
+        password = request.form["password"]
+
+        
--- db/models.py
+++ db/models.py
@@ -25,3 +25,19 @@ def getRangeProducts(minPrice, maxPrice):
     connection.close()
 
     return result
+
+
+def getUser(login, password):
+    connection = sqlite3.connect('base.db')
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        SELECT *
+        FROM users
+        WHERE login = ? AND password = ?
+    ''',(login, password))
+    result = cursor.fetchone()
+
+    connection.close()
+    
+    return result
